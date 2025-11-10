d366e339d336ed9930aa4c3a91672de22db8ff1f
sunminn-byte <sunminn0627@gamil.com>
2025-11-10 18:08:17 +0900

    251110 route~

---
+++ app.js
@@ -0,0 +1,80 @@
+import express, { response } from 'express'; // express 모듈 가져오기
+
+const app = express();
+
+// app.get(); : http method가 get인거만 가져옴
+// request : 유저가 보내온 모든 parameter들이 담김(요청)
+// response : 유저에게 줄 정보를 셋팅해서 전달(응답)
+// next : 미들웨어(다음처리로 이동할 때)
+
+// 클라이언트가 '/api/hi' 경로로 GET 요청을 보낼 때 실행되는 Router
+app.get('/api/hi', (request, response, next) => {
+  response.status(200).send({
+    code: '00',
+    msg: '안녕 익스프레스!'
+  });
+});
+
+// 클라이언트가 '/api/hi' 경로로 POST 요청을 보낼 때 실행되는 Router
+app.post('/api/hi', (request, response, next) => {
+  response.status(200).send('포스트 익스프레스!');
+});
+
+// 클라이언트가 '/api/hi' 경로로 PUT 요청을 보낼 때 실행되는 Router
+app.put('/api/hi', (request, response, next) => {
+  response.status(200).send('풋 익스프레스!');
+});
+
+// 클라이언트가 '/api/hi' 경로로 DELETE 요청을 보낼 때 실행되는 Router
+app.delete('/api/hi', (request, response, next) => {
+  response.status(200).send('딜리트 익스프레스!');
+});
+
+
+// ---------------------------------------------
+// Query Parameter 제어 (RESTful API에서 사용X)
+// Request.query 프로퍼티를 통해서 접근 가능
+// 모든 값을 string으로 받기 때문에 주의 필요
+app.get('/api/posts', (request, response, next) => {
+  const params = request.query;
+  const name = request.query.name;
+  const age = parseInt(request.query.age); // 백엔드에는 무조건 string으로 넘어오기 때문에 parseInt
+
+  // console.log(name, age); // 서버에서 확인 가능
+  response.status(200).send(params); // 포스트맨에서 확인 가능
+});
+
+// ---------------------------------------------
+// Segment Parameter
+// `Request.params` 를 통해서 접근 가능
+app.get('/api/posts/:id', (request, response, next) => {
+  const postId = request.params.id; // 위의 :뒤에 있는거랑 동일하게 적어줘야 함(/:id -> .id)
+  console.log(typeof(postId));
+  response.status(200).send(postId);
+});
+
+
+// ---------------------------------------------
+// 대체 라우트(모든 라우터 중에 가장 마지막에 작성)
+// 위에서 정하지 않은 나머지 모든 경로
+// use() : 모든 경로
+// 위와 다르게 포트 설정 없음
+// 미들웨어의 일종
+app.use((request, response, next) => {
+  response.status(404).send({
+    code: 'E01',
+    msg: '찾을 수 없는 페이지입니다.'
+  });
+});
+
+
+// 서버를 주어진 포트에서 시작
+// app.listen(3000);
+// 3000은 임의로 사용 가능(이 컴퓨터내에서 이미 사용중인 번호는 중복 사용 불가)
+// 최초에 서버를 시작할 때
+// 유저가 언제 시작할 지 모르기때문에 항상 켜져있어야 함
+// 첫번째 : 시작할 포트 number
+// 두번째 : hostname, 보통 콜백함수로, 요청할거 없으면 사용x
+app.listen(3000, () => {
+  console.log(`3000포트에서 리스닝`);
+});
